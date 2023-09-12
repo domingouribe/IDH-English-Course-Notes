f0d519d42da31548c9c31a2fa60da7cf1a37899a
Domingo Uribe <uribe.domingo@gmail.com>
2023-09-12 09:16:15 -0300

    The fors backup with regular and irregular nouns

---
+++ notes.js
@@ -0,0 +1,74 @@
+//Phrases 
+
+//Regular nouns
+// There IS an umbrella on the table
+
+// There ARE umbrellas in the bedroom
+
+// When is singular form, always use: There is
+//And when is plural is not neccesary use the article
+const regular_nouns = [
+    'There IS an umbrella on the table',
+    'There ARE umbrellas in the bedroom',
+    'There is a pen in the backpack',
+    'There are (a lot of) [or many] pens in my backpack',
+    'There are many pens in my backpack',
+    'There are 4 chairs in the classroom',
+    'There are 15 pictures on the wall',
+    'There is a windows behind me',
+    'There are many pisctures on the wall',
+    'There is a toy house on the closet',
+    'There is a Teddy Bear on the couch',
+    'There are books on the bookcase',
+    'There ir a green chair in the room [Room is correwct]',
+    'Furniture = Mueble',
+    'There are two teddy bears under the couch',
+    'There is a piggy back on the desk',
+    'There is a lamp on the wall'
+]
+
+console.log(regular_nouns[0])
+
+const questions = [
+    'Where is the rug? = There is a rug on the floor',
+    'Where is the alarm clock? = There is an alarm clock on the shelf (repisa)',
+    'How many ball are there in the bedroom? = There are three balls in the bedroom',
+    'Where are the pillows (or cushion)? = There are two pillows (Almohadones o cojines) on the couch',
+    'Where is the plant? = There is a plant on the shelf',
+    'The plant is next to the photo',
+    'Where are the clothes? = There are clothes in the closet',
+    'There is not a bed in the bedroom'
+];
+
+console.log(questions[4])
+
+//Preposicions on place
+//Through = através de
+
+//Se puede preguntar cuántos hay, o dónde están
+// Regular nouns have "s" in the fnal
+
+/*
+
+Negative sentenses_
+
+-EROR: There are behind you a clock on the wall? 
+-Correct: Is there a clock on the wall?
+- Yes. There is a clock on the wall || Yes, there is
+
+/*
+September 12
+What's the topic today?
+
+Regular nouns and irregular nouns
+
+Reconigze when regular nouns or irregular nouns
+
+Mouse > Mice: Mice es the irregular form of mouse noun, in plural (The animal as the object it's same)
+
+> There ir a mouse ('mauss') in the bedroom
+> There are mice ('mais') in the bedroom 
+
+
+
+*/
