cd4d3b621659efaefa156a61215aa6ef518b5ef1
Domingo Uribe <uribe.domingo@gmail.com>
2023-09-12 10:17:12 -0300

    More content

--- notes.js
+++ notes.js
@@ -69,6 +69,95 @@ Mouse > Mice: Mice es the irregular form of mouse noun, in plural (The animal as
 > There ir a mouse ('mauss') in the bedroom
 > There are mice ('mais') in the bedroom 
 
+Conformate sentences oral
+
+> There are 26 students in this class
+> There are two clocks in mi house
+
+Demostrative adjectives:
+There are two clocks in my house.
+This (Esto), These(Esos), that(Eso), those(Estos).
+
+
+
+Dialog for take note about different information:
+
+i still lokkin for apartment?
+yes, it i am. S till looking
+
+great kitchen
+
+forget these! 
+
+Are you interesrted in appartment?
+Yes, im very interested
+
+tell me how many bedroom have the appartment? 
+thre is the wash machine ?
+
+
+> Looking for (buscar o buscando) an Apartment
+> There is / There Are
+> Singular / Plural
+
+Yes. Thes a very nice apartment por rest in my building now
+Really, tell me about it
+
+There a large window in the livingroom and there's a big closet (clast) in the bedroom
+
+There's a great kitchen
+Is it larg? 
+Yes, there's room a nice big table
+
+Is there an elevator (eleveidor) in the building?
+No, there isn't.
+
+But, get this there's a Jacuzzi in the bathroom
+NO, youo are kidding
+No, I'm serious
+
+Are you interested in the aparment?
+Yes, i'm very interested
+
+
+Next one dialog
+
+Is the stove(cocina) in the kitchen?
+Yes, there is (Respuesta afirmativa debe ser sin contracción).There's nice stove in the kitchen
+
+
+Next one:
+Is there a refrigerator in the kitchen?
+No, there isn't
+
+
+
+Next one:
+> Tell me, how many bedroom are there (this is the correct order) in the apartment?
+> There are two bedrooms (regular noun in plural) in the apartment
+> Two bedrooms?
+> Thats right! (Estás en lo correcto)
+> Tell me, are there many closets in the apartment?
+> Yes, there are (Respuesta afirmativa). There are three very larg closets
+> Are there washing machine in the building?
+> No there aren't, but there's a laundromat around the corner (Pero hay una lavandería en la esquina)
+> Are you interested
+> Yes, I am
+
+[
+    Cuantificadores: Se usan con there are, porque son plurales
+A lot of: demasiados
+Many: muchos
+Several: varios
+
+In singular, we use a or an, or numer one when there are a cuestion
+
+>Hoy many bedroom are there in the bedroom?
+>There is one bedroom
+
+]
+
+
 
 
 */
